5682b75874b68b1f7db99e6d44e0429c18e341af
porqueEUprogramo <porqueeuprogramo@gmail.com>
2023-03-22 23:43:39 +0000

    implementation menu crud

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from "@/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return  <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
+  const queryClient = new QueryClient();
+
+  return <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </QueryClientProvider>
 }
